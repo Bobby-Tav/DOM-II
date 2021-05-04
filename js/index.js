0c884b96b06bbe3f897afa3d5d064bc783a9b994
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-05-04 18:45:47 -0400

    Header done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,80 @@
 // Your code goes here
+const header = document.querySelector('header')
+const nav = document.querySelector('nav');
+const firstSec = document.querySelector('content-section');
+
+//nav
+const navLink = document.querySelector('.nav-link');
+
+const home = document.querySelector('.home')
+
+//first section
+//Header - change color
+header.addEventListener('mouseover', function(event){
+    header.style.backgroundColor = "#48dbe0";
+    header.style.transition = '2s'
+});
+
+header.addEventListener('mouseout', function(event){
+    header.style.backgroundColor = "#fff";
+    header.style.transition = '2s'
+});
+
+//Nav link 
+nav.children[0].addEventListener('mouseover', event => {
+    nav.children[0].style.fontSize = '2rem';
+    nav.children[0].style.transition = '1s'
+}); 
+
+nav.children[0].addEventListener('mouseout', event => {
+    nav.children[0].style.fontSize= '1.6rem';
+    nav.children[0].style.transition = '1s'
+}); 
+nav.children[1].addEventListener('mouseover', event => {
+    nav.children[1].style.fontSize = '2rem';
+    nav.children[1].style.transition = '1s'
+}); 
+
+nav.children[1].addEventListener('mouseout', event => {
+    nav.children[1].style.fontSize= '1.6rem';
+    nav.children[1].style.transition = '1s'
+});
+
+nav.children[2].addEventListener('mouseover', event => {
+    nav.children[2].style.fontSize = '2rem';
+    nav.children[2].style.transition = '1s'
+}); 
+
+nav.children[2].addEventListener('mouseout', event => {
+    nav.children[2].style.fontSize= '1.6rem';
+    nav.children[2].style.transition = '1s'
+}); 
+nav.children[3].addEventListener('mouseover', event => {
+    nav.children[3].style.fontSize = '2rem';
+    nav.children[3].style.transition = '1s'
+}); 
+
+nav.children[3].addEventListener('mouseout', event => {
+    nav.children[3].style.fontSize= '1.6rem';
+    nav.children[3].style.transition = '1s'
+});
+
+nav.children[4].addEventListener('mouseover', event => {
+    nav.children[4].style.fontSize = '2rem';
+    nav.children[4].style.transition = '1s'
+}); 
+
+nav.children[4].addEventListener('mouseout', event => {
+    nav.children[4].style.fontSize= '1.6rem';
+    nav.children[4].style.transition = '1s'
+});
+
+//container home
+
+home.addEventListener('wheel', event => {
+    debugger;
+})
+
+
+
+

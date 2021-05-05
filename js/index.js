0862b2a6f301ccbda079c1c2ee3ab94c1e104adc
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-05-04 20:59:05 -0400

    done

--- js/index.js
+++ js/index.js
@@ -2,13 +2,15 @@
 const header = document.querySelector('header')
 const nav = document.querySelector('nav');
 const firstSec = document.querySelector('content-section');
-
 //nav
 const navLink = document.querySelector('.nav-link');
+const bus = document.querySelector('.intro img');
+const map = document.querySelector('.img-content img')
+const title = document.querySelector('.content-section h2')
+const footer = document.querySelector('footer')
 
-const home = document.querySelector('.home')
 
-//first section
+
 //Header - change color
 header.addEventListener('mouseover', function(event){
     header.style.backgroundColor = "#48dbe0";
@@ -59,21 +61,50 @@ nav.children[3].addEventListener('mouseout', event => {
     nav.children[3].style.transition = '1s'
 });
 
-nav.children[4].addEventListener('mouseover', event => {
-    nav.children[4].style.fontSize = '2rem';
-    nav.children[4].style.transition = '1s'
-}); 
 
-nav.children[4].addEventListener('mouseout', event => {
-    nav.children[4].style.fontSize= '1.6rem';
-    nav.children[4].style.transition = '1s'
+
+//Coping
+document.addEventListener('copy', event => {
+    alert("What are you doing!?");
+})
+
+
+//making an img into a circle
+bus.addEventListener('click', event => {
+
+    bus.style.borderRadius = "50%";
+    bus.style.marginTop = "3%";
+    bus.style.transition = "2s";
 });
 
-//container home
-
-home.addEventListener('wheel', event => {
-    debugger;
+window.addEventListener('load', event =>{
+    alert("Hello, Im Al the A.I");
 })
+
+map.addEventListener('dblclick', event => {
+    map.style.opacity = "0.2";
+    map.style.transition = "2s";
+});
+
+map.addEventListener('transitionend', event => {
+    map.style.opacity = "1";
+    map.style.transition = "2s";
+});
+
+title.addEventListener('mousemove', event => {
+    title.style.fontSize = '5rem';
+});
+
+footer.addEventListener('mouseenter', event => {
+    footer.style.backgroundColor = '#d8f589';
+})
+
+footer.addEventListener('mouseleave', event => {
+    footer.style.backgroundColor = '';
+})
+
+
+
 
 
 
